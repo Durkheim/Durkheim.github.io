dd0420eda906183d86f8b000827877d355eb68fd
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-02-09 15:47:07 -0600

    creating webpage for game

--- projects/rock_paper_scissors.js
+++ projects/rock_paper_scissors.js
@@ -1,98 +1,48 @@
-do {
-
-var player = {
-  name: "You",
-  status: "newbie",
-  money: 0,
-  choice: prompt("Enter rock, paper, or scissors (entry must be all lower case):"),
+var user;
+var choose = function(choice) {
+    user = choice;
 }
 
-var gameArray = [
- "rock",
- "paper",
- "scissors"
-];
-
-var computer = {
-  name: "The Iron Knight",
-  status: "champion",
-  money: 1000,
-  choice: gameArray[Math.floor(Math.random() * gameArray.length)]
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+}
+else if (computerChoice < 0.67) {
+    computerChoice = "paper";
+}
+else {
+    computerChoice = "scissors";
 }
 
-console.log(player.name + " chose " + player.choice + ".");
-console.log(computer.name + " chose " + computer.choice + ".");
-
-var compare = function (playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-     return ("Tie.");
-  }
-
-  else if (playerChoice === "paper") {
-    if (computerChoice === "rock") {
-      return ("You win!");
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        document.getElementById("result").innerHTML = "Tie!";
     }
-
-    else {
-      return ("You lose!");
+    else if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
+        else {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
     }
-  }
-
-  else if (playerChoice === "rock") {
-    if (computerChoice === "scissors") {
-          return ("You win!");
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
+        else if (choice2 ==="scissors") {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
     }
-    else {
-      return ("You lose!");
-    }
-  }
-
-  else if (playerChoice === "scissors") {
-    if (computerChoice === "paper") {
-      return ("You win!");
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            document.getElementById("result").innerHTML = "You Lose!";
+        }
+        else if (choice2 === "paper") {
+            document.getElementById("result").innerHTML = "You Win!";
+        }
     }
     else {
-      return("You lose!");
+        document.getElementById("result").innerHTML = "ERROR!";
     }
-  }
-
 }
-
-console.log(compare(player.choice, computer.choice));
-
-if (compare(player.choice, computer.choice) === "You win!") {
-  player.status = "champion";
-  computer.status = "loser";
-  console.log(player.name + " are the " + player.status);
-  console.log(computer.name + " is the " + computer.status);
-}
-
-else if (compare(player.choice, computer.choice) === "You lose!") {
-  player.status = "loser";
-  computer.status = "champion";
-  console.log(player.name + " are the " + player.status + ".");
-  console.log(computer.name + " is the " + computer.status + "!");
-}
-
-else if (compare(player.choice, computer.choice) === "Tie.") {
- console.log("The match was a draw, so " + player.name + " are still the " + player.status);
- console.log("The match was a draw, so " + computer.name + " is still the " + computer.status);
-}
-
-var stealMoney = function(playerStatus) {
-  if (playerStatus === "champion") {
-    player.money = player.money + 10;
-    computer.money = computer.money - 10;
-  }
-}
-
-stealMoney(player.status);
-
-console.log(player.name + " stole " + player.money + " coins.");
-
-
-var playAgain = prompt("Do you want to play again (yes/no)?");
-
-}
-
-while (playAgain === "yes");
